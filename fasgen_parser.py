ff4c337a172ac2a703962845f877ff1536def85e
Andrew Bright <legendarkakr@gmail.com>
2024-09-15 23:46:18 +0300

    dsfdw

--- fasgen_parser.py
+++ fasgen_parser.py
@@ -44,7 +44,7 @@ def init_browser():
     if driver is None:
         driver = webdriver.Chrome(service=service, options=chrome_options)
         cookies = driver.get_cookies()
-        cookie = {
+        cookies = {
             'name': '_ga_7QY9GCL0DC',
             'value': 'GA1.1.1435541569.1726432939',  # Замените на ваше значение
             'domain': '.fastflux.ai'  # Укажите нужный домен
