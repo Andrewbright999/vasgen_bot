4a0592403784c2fbb6c75e9c5b1ffa55f7a05651
Andrew Bright <legendarkakr@gmail.com>
2024-09-15 23:01:55 +0300

    parser changed

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv() 
 
 TG_TOKEN = os.getenv("TG_TOKEN")
+# You are connected to database "korobka" as user "korobka" via socket in "/var/run/postgresql" at port "5432".
--- fasgen_parser.py
+++ fasgen_parser.py
@@ -54,8 +54,7 @@ def generate_image(prompt: str):
             init_browser()
 
         # Находим поле для ввода текста для промта
-        print(" Находим поле для ввода текста для промта")
-        prompt_input = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[1]/div/div/textarea')
+        prompt_input = driver.find_element(By.CSS_SELECTOR, '#root > div > div > div.flex.flex-col.items-center.w-full.max-w-\[600px\].flex-none > div > div > textarea')
 
         # Очищаем поле ввода
         prompt_input.clear()
@@ -64,43 +63,35 @@ def generate_image(prompt: str):
         prompt_input.send_keys(prompt)
 
         # Находим кнопку для генерации изображения и кликаем на неё
-        generate_button = driver.find_element(By.XPATH, '//*[@id="submit-btn-5dt"]')
-        print("Находим кнопку для генерации изображения и кликаем на неё")
+        generate_button = driver.find_element(By.CSS_SELECTOR, '#submit-btn-5dt')
         generate_button.click()
-        print("Клик")
-        
 
         # Ждем генерацию изображения
         time.sleep(3)  # Увеличиваем время ожидания, если это необходимо
 
         # Находим кнопку для скачивания изображения
-        print("Находим кнопку для скачивания изображения")
-        
-        download_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/button')
-
+        download_button = driver.find_element(By.CSS_SELECTOR, '#root > div > div > div.hidden.md\:block.max-w-\[1050px\].w-full.relative > button')
         download_button.click()
-        print("Скачал")
-        
 
         # Ждем, пока изображение будет загружено
         time.sleep(3)  # Увеличиваем время ожидания
-        
+
         # Путь к загруженному файлу
         files = os.listdir(download_folder_path)
-        print(files)
         for file in files:
             if file.endswith(".webp"):  # Предполагаем, что исходный файл имеет расширение .webp
                 old_file_path = os.path.join(download_folder_path, file)
                 return old_file_path
-                # new_file_path = os.path.join(download_folder_path, "generated_image.jpeg")
+                
+                new_file_path = os.path.join(download_folder_path, "generated_image.jpeg")
 
-                # # Открываем изображение и сохраняем его в формате JPEG
-                # with Image.open(old_file_path) as img:
-                #     img.convert("RGB").save(new_file_path, "JPEG")
+                # Открываем изображение и сохраняем его в формате JPEG
+                with Image.open(old_file_path) as img:
+                    img.convert("RGB").save(new_file_path, "JPEG")
 
-                # # Удаляем исходный файл (если нужно)
-                # os.remove(old_file_path)
-                # return new_file_path
+                # Удаляем исходный файл (если нужно)
+                os.remove(old_file_path)
+                return new_file_path
 
     except Exception as e:
         print(f"Ошибка при создании изображения: {e}")
